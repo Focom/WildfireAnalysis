bd7b26c03376002fdaa4fca97302f895d4f1ba82
Pierre VALENTIN <pievalentin@gmail.com>
2017-11-24 17:56:33 -0500

    Can read the sqlite file

---
+++ MongoConverter/converter.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+
+    // SQLite only
+    storage: 'FPA_FOD_20170508.sqlite'
+})
+
+sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
