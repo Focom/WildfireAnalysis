ff2a8ad40dd28419ac97362dd0d6a1878c52005e
Pierre VALENTIN <pievalentin@gmail.com>
2017-12-06 22:37:53 -0500

    The schema for the other table we might need it later

---
+++ Mongo/otherSchema.js
@@ -0,0 +1,17 @@
+import { Mongoose } from "mongoose";
+
+let UnitIDActive = Mongoose.Schema({
+NWCG_UnitIDActive_20170109,
+UnitId,
+GeographicArea,
+Gacc,
+WildlandRole,
+UnitType,
+Department,
+Agency,
+Parent,
+Country,
+State,
+Code,
+Name
+})
