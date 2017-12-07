71ad58bd596fd9da7be97265da25aaac8fec65cb
Pierre VALENTIN <pievalentin@gmail.com>
2017-12-07 16:34:19 -0500

    Command to import the json file in the database

---
+++ DataSet/importDocuments.sh
@@ -0,0 +1,26 @@
+
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1992.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1993.json --jsonArray  
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1994.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1995.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1996.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1997.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1998.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/1999.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2000.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2001.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2002.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2003.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2004.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2005.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2006.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2007.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2008.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2009.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2010.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2011.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2012.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2013.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2014.json --jsonArray
+mongoimport --db Datasets --collection Fires --file ./DataJSON/2015.json --jsonArray
+
