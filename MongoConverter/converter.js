fa9ec6d0370593fbff9c37bdd1c9e991bb9a3921
Pierre VALENTIN <pievalentin@gmail.com>
2017-12-06 22:37:22 -0500

    Working retrieval need to work to remove shape key and check style of json

--- MongoConverter/converter.js
+++ MongoConverter/converter.js
@@ -1,25 +1,48 @@
+
+var fs = require('fs')
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
+  host: 'localhost',
+  dialect: 'sqlite',
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
 
     // SQLite only
-    storage: 'FPA_FOD_20170508.sqlite'
+  storage: './DataSet/FPA_FOD_20170508.sqlite'
 })
 
 sequelize
 .authenticate()
 .then(() => {
-  console.log('Connection has been established successfully.');
+  console.log('Connection has been established successfully.')
 })
 .catch(err => {
-  console.error('Unable to connect to the database:', err);
-});
+  console.error('Unable to connect to the database:', err)
+})
 
+function query () {
+  return new Promise(resolve => {
+    sequelize.query('SELECT * FROM Fires WHERE FIRE_YEAR=2005').then(Fires => {
+      resolve(Fires)
+    })
+  })
+}
+
+async function writeToFile () {
+  let value = await query()
+  fs.writeFile('test.json', JSON.stringify(value), function (err) {
+    if (err) {
+      return console.log(err)
+    }
+
+    console.log('The file was saved!')
+  })
+}
+
+// testWriting()
+writeToFile()
