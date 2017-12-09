da084390205c035a931b34fa265fb6a47d73f5d1
Pierre VALENTIN <pievalentin@gmail.com>
2017-12-09 18:52:14 -0500

    Uncomment a line to make ready to use

--- DataSet/converter.js
+++ DataSet/converter.js
@@ -56,7 +56,7 @@ function createAllJSON () {
 }
 
 //First be sure to have the FPA_FOD_20170508.sqlite file in this folder 
-// Call this function to create the required json to populate the mongodatabase
-// createAllJSON()
+// Call once this function to create the required json to populate the mongodatabase
+createAllJSON()
 
 // Once done execute the importDocuments script, your mongodb will be ready in seconds.
